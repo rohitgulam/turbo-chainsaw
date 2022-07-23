e4a2cce5d32b6bf0d503c2ee61a0c74ef5c9861f
Rohit Gulam <rohitsgulam@gmail.com>
2022-07-23 23:01:55 +0300

    :sparkles: added image area to home view

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Search from '../components/Search';
+import ImageArea from '../components/ImageArea';
 
 function Home() {
   return (
     <div>
       <Navbar/>
       <Search/>
+      <ImageArea/>
     </div>
   )
 }
