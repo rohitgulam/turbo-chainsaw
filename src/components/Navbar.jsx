c498687b759667da1bbd0ef8fdefea4f87320d84
Rohit Gulam <rohitsgulam@gmail.com>
2022-07-23 22:59:50 +0300

    chnaged app name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Navbar() {
   return (
     <nav className='container flex justify-between items-center mx-auto py-5 px-8'>
-      <a href="#" className="font-bold text-4xl text-gray-800">Photo</a>
+      <a href="#" className="font-bold text-4xl text-gray-800">Unnamed App 1</a>
     </nav>
   )
 }
