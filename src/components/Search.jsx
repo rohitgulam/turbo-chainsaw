09b3c7104f9beb3ba2ddb71196c90fcd994ec1d3
Rohit Gulam <rohitsgulam@gmail.com>
2022-07-23 23:00:18 +0300

    created search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -9,13 +9,10 @@ function Search() {
             <span class="absolute inset-y-0 left-0 flex items-center px-5">
                 <ImSearch className='fill-slate-500'/> 
             </span>
-            <input type="text" name="link" id="link" placeholder='Paste Twitter/Telegram link ' className='w-full mx-auto py-3 px-12 border-2 border-slate-300 rounded-lg text-xl shadow-2xl focus:outline-none w-96 placeholder:text-slate-500'/>
+            <input type="text" name="link" id="link" placeholder='Paste Twitter/Telegram link' className='w-[100%] mx-auto py-3 px-12 border-2 border-slate-300 rounded-lg text-xl shadow-2xl focus:outline-none w-96 placeholder:text-slate-500'/>
             <span class="absolute inset-y-0 right-0 flex items-center px-5">
                 <FaRegTimesCircle className='fill-slate-500 text-xl'/> 
             </span>
-            {/* <input
-                class="w-full bg-white placeholder:font-italitc border border-slate-300 rounded-full py-2 pl-10 pr-4 focus:outline-none"
-                placeholder="Enter your keyword to search" type="text" /> */}
     </form>
   )
 }
