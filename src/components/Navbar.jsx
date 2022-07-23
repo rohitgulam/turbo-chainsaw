2181856a85d0c5c2e5755562dfb54b24cd3baec6
Rohit Gulam <rohitsgulam@gmail.com>
2022-07-23 20:58:11 +0300

    created navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Navbar() {
+  return (
+    <nav className='container flex justify-between items-center mx-auto py-5 px-8'>
+      <a href="#" className="font-bold text-4xl text-gray-800">Photo</a>
+    </nav>
+  )
+}
+
+export default Navbar
