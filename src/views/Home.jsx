ff6915be2834345c8296affc1314cff7bd37b486
Rohit Gulam <rohitsgulam@gmail.com>
2022-07-23 20:58:49 +0300

    created home view

---
+++ src/views/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Search from '../components/Search';
+
+function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <Search/>
+    </div>
+  )
+}
+
+export default Home
