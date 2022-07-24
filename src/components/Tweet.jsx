d6a922c8fbb4e537e39ad0e48c910a8ae70301ad
Rohit Gulam <rohitsgulam@gmail.com>
2022-07-24 09:36:36 +0300

    :iphone: tweet padding and size responsiveness

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -3,14 +3,17 @@ import img from './img.jpg'
 
 function Tweet() {
   return (
-    <div className='flex flex-col justify-evenly w-3/4 min-h-1/2 py-8 px-16 bg-white shadow-2xl rounded-2xl'>
+    <div className='flex flex-col justify-evenly w-full min-h-1/2 py-8 px-6 bg-white shadow-2xl rounded-2xl md:w-3/4 md:px-16 '>
         <div className='flex items-center'>
             <img src={img} alt="" className='w-10 h-10 rounded-full' />
             <h2 className='ml-4 font-medium text-xl' >Rohit Gulam</h2>
         </div>
         <div>
             <p className='leading-9 text-xl py-5'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit placeat hic esse nam nisi omnis consequatur commodi, molestiae harum sed dolore non, unde architecto cum iste! Exercitationem voluptas quidem consectetur Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum dolores veniam sint consectetur, deserunt commodi architecto quidem. Rerum asperiores mollitia itaque ad, ea, qui praesentium doloremque illum dicta cupiditate tempora illo voluptatem deleniti. Magnam esse commodi quasi explicabo minus ex?
+            Women. <br /> 
+            Your best friend (who is single) is the #1 cause of all your relationship troubles. Stop listening to her advice about men. Listen to your heart. 
+            <br />
+            Sincerely, all men.
             </p>
         </div>
         <div className='flex text-slate-500' >
