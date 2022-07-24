551c317e7eb8832d558091e222b67b12d3a57d37
Rohit Gulam <rohitsgulam@gmail.com>
2022-07-24 08:48:21 +0300

    : iphone: logo to display in the middle on small devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <nav className='container flex justify-between items-center mx-auto py-5 px-8'>
+    <nav className='container flex justify-center items-center mx-auto py-5 px-8 md:justify-between'>
       <a href="#" className="font-bold text-4xl text-gray-800">Unnamed App 1</a>
     </nav>
   )
