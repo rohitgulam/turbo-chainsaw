49f80d9541eaebf9863db00cc6c3a82385ca5c9a
Rohit Gulam <rohitsgulam@gmail.com>
2022-07-24 08:58:15 +0300

    :iphone: full form on small devices and half on md+

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,13 +4,18 @@ import {FaRegTimesCircle} from 'react-icons/fa';
 
 function Search() {
   return (
-    <form className="relative w-1/3 mx-auto my-5">
+    <form className="relative w-full my-5 px-6 md:w-1/2 md:mx-auto">
         
-            <span class="absolute inset-y-0 left-0 flex items-center px-5">
+            <span class="absolute inset-y-0 left-6 flex items-center px-5">
                 <ImSearch className='fill-slate-500'/> 
             </span>
-            <input type="text" name="link" id="link" placeholder='Paste Twitter/Telegram link' className='w-[100%] mx-auto py-3 px-12 border-2 border-slate-300 rounded-lg text-xl shadow-2xl focus:outline-none w-96 placeholder:text-slate-500'/>
-            <span class="absolute inset-y-0 right-0 flex items-center px-5">
+            <input 
+              type="text" 
+              name="link" 
+              id="link" 
+              placeholder='Paste Twitter/Telegram link' 
+              className='w-[100%] mx-auto py-3 px-12 border-2 border-slate-300 rounded-lg text-xl shadow-2xl focus:outline-none placeholder:text-slate-500'/>
+            <span class="absolute inset-y-0 right-6 flex items-center px-5">
                 <FaRegTimesCircle className='fill-slate-500 text-xl'/> 
             </span>
     </form>
