a05cd27cdf7e1423b1d572f93bb058c2e4ce6970
Rohit Gulam <rohitsgulam@gmail.com>
2022-07-24 09:37:21 +0300

    :iphone: responsive for image area (padding, height, width )

--- src/components/ImageArea.jsx
+++ src/components/ImageArea.jsx
@@ -3,7 +3,8 @@ import Tweet from './Tweet';
 
 function ImageArea() {
   return (
-    <section id="image" className="container flex justify-center items-center mx-auto bg-gradient-to-r from-blue-300 to-blue-400 mt-[3rem] h-[38rem] w-[70rem] shadow-2xl">
+    <section id="image" 
+    className="container flex justify-center items-center mx-auto bg-gradient-to-r from-blue-300 to-blue-400 mt-[3rem] px-5 min-h-[34rem] max-w-[70rem] shadow-2xl">
         <Tweet/>
     </section>
   )
