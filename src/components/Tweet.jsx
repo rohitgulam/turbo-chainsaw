cc96cbb52c1242894ee39d40517936d4917f7964
Rohit Gulam <rohitsgulam@gmail.com>
2022-07-24 20:46:27 +0300

    :iphone: created dark and stats toggle

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import img from './img.jpg'
+import img from './img.jpg';
+import { useContext } from 'react';
+import OptionContext from '../OptionContext';
 
 function Tweet() {
+    const {stats} = useContext(OptionContext);
+    const {dark} = useContext(OptionContext);
   return (
-    <div className='flex flex-col justify-evenly w-full min-h-1/2 py-8 px-6 bg-white shadow-2xl rounded-2xl md:w-3/4 md:px-16 '>
+    <div className={dark ? 'flex flex-col justify-evenly w-full min-h-1/2 py-8 px-6  shadow-2xl rounded-2xl md:w-3/4 md:px-16 bg-gray-900 text-white': 'flex flex-col justify-evenly w-full min-h-1/2 py-8 px-6  shadow-2xl rounded-2xl md:w-3/4 md:px-16 bg-white text-black'}>
         <div className='flex items-center'>
             <img src={img} alt="" className='w-10 h-10 rounded-full' />
             <h2 className='ml-4 font-medium text-xl' >Rohit Gulam</h2>
@@ -23,7 +27,7 @@ function Tweet() {
             <p className='mr-2'>3:25PM</p>
             <p>01.01.2021</p>
         </div>
-        <div className='flex justify-between mt-3 text-slate-500 w-1/2' >
+        <div className={ stats ? 'flex justify-between mt-3 text-slate-500 w-1/2' : 'hidden'} >
             <p className=''>12 replies</p>
             <p> 24 retweets</p>
             <p> 39 likes</p>
