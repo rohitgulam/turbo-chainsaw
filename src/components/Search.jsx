44020df11e80300c95e67d6ff9183a4ef4728373
Rohit Gulam <rohitsgulam@gmail.com>
2022-07-24 13:26:39 +0300

    :bug: fixed icons spans from class to classname

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,7 +6,7 @@ function Search() {
   return (
     <form className="relative w-full my-5 px-6 md:w-1/2 md:mx-auto">
         
-            <span class="absolute inset-y-0 left-6 flex items-center px-5">
+            <span className="absolute inset-y-0 left-6 flex items-center px-5">
                 <ImSearch className='fill-slate-500'/> 
             </span>
             <input 
@@ -15,7 +15,7 @@ function Search() {
               id="link" 
               placeholder='Paste Twitter/Telegram link' 
               className='w-[100%] mx-auto py-3 px-12 border-2 border-slate-300 rounded-lg text-xl shadow-2xl focus:outline-none placeholder:text-slate-500'/>
-            <span class="absolute inset-y-0 right-6 flex items-center px-5">
+            <span className="absolute inset-y-0 right-6 flex items-center px-5">
                 <FaRegTimesCircle className='fill-slate-500 text-xl'/> 
             </span>
     </form>
